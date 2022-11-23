5f9009a7ff374649425d4a934269e4e472ebd9f0
afridi <afridi@tikweb.com>
2022-11-23 13:57:19 +0600

    Code refactoring

--- lib/controller/frame_controller.dart
+++ lib/controller/frame_controller.dart
@@ -2,22 +2,31 @@ import 'package:get/get.dart';
 
 class FrameController extends GetxController {
   var isBudgetFloatingActionButtonModuleOpen = false.obs;
+  var startBudgetFloatingActionButtonAnimation = false.obs;
+
   var selectedFrameIndex = 0.obs;
   var currentFrameIndex = 0.obs;
-  var startBudgetFloatingActionButtonAnimation = false.obs;
 
   changeBudgetFloatingActionButtonStatus(bool status) async {
     if (status) {
-      isBudgetFloatingActionButtonModuleOpen.value = status;
-      await Future.delayed(const Duration(milliseconds: 100)).then((_) {
-        startBudgetFloatingActionButtonAnimation.value = true;
-      });
+      await _openBudgetPopupModule(status);
     } else {
-      startBudgetFloatingActionButtonAnimation.value = status;
-      await Future.delayed(const Duration(milliseconds: 100)).then((_) {
-        isBudgetFloatingActionButtonModuleOpen.value = status;
-        selectedFrameIndex.value = currentFrameIndex.value;
-      });
+      await _closeBudgetPopupModule(status);
     }
+  }
+
+  Future<void> _closeBudgetPopupModule(bool status) async {
+    startBudgetFloatingActionButtonAnimation.value = status;
+    await Future.delayed(const Duration(milliseconds: 100)).then((_) {
+      isBudgetFloatingActionButtonModuleOpen.value = status;
+      selectedFrameIndex.value = currentFrameIndex.value;
+    });
+  }
+
+  Future<void> _openBudgetPopupModule(bool status) async {
+    isBudgetFloatingActionButtonModuleOpen.value = status;
+    await Future.delayed(const Duration(milliseconds: 100)).then((_) {
+      startBudgetFloatingActionButtonAnimation.value = true;
+    });
   }
 }
